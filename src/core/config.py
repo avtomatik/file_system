787775c591f747cd1ed028c2c59cade8237d8d5d
Alexander Mikhailov <extroper@yandex.ru>
2025-05-08 16:43:10 +0300

    Introduced Environment Variables

--- src/core/config.py
+++ src/core/config.py
@@ -6,27 +6,16 @@ Created on Wed May  7 20:00:00 2025
 @author: alexandermikhailov
 """
 
+import os
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+from dotenv import load_dotenv
 
-DIR = '/Users/alexandermikhailov/Documents'
-DIR = '/Volumes/NO NAME'
-DIR = '/Volumes/NO NAME 1'
-DIR = '/Volumes/Samsung USB'
-DIR = '/Volumes/Samsung USB/production/egida_ptv/'
-DIR = '/Volumes/Samsung USB/web/egida_ptv/'
-DIR = '/home/green-machine/Downloads'
-DIR = '/home/green-machine/production'
-DIR = '/home/green-machine/source'
-DIR = '/home/green-machine/web'
-DIR = '/media/green-machine/321B-6A94'
-DIR = '/media/green-machine/904F-3DB1'
-DIR = '/media/green-machine/904F-3DB1/reports/latex'
-DIR = '/media/green-machine/KINGSTON'
-DIR = '/media/green-machine/Transcend'
+load_dotenv()
 
+DIR = os.getenv('DIR')
 PATH = Path(DIR)
+
 PATH_CTR = Path(DIR)
 PATH_DST = Path(DIR)
 PATH_SRC = Path(DIR)
