8a0a9beb1397f5a9a2152b99e235d7367c3ceb79
Alexander Mikhailov <extroper@yandex.ru>
2025-05-12 17:03:19 +0300

    Update Walk Some

--- src/walk_some.py
+++ src/walk_some.py
@@ -1,27 +1,17 @@
-import os
-import shutil
-from pathlib import Path
-
 from core.config import PATH_DST, PATH_SRC
 from core.constants import PREFIXES
 
 SUFFIX = '.py'
 
+for path in PATH_SRC.rglob('*'):
+    if path.is_file():
+        file_name = path.name
 
-for root, _, file_names in os.walk(PATH_SRC):
-    for file_name in file_names:
+        if file_name.endswith(SUFFIX) and not file_name.startswith(tuple(PREFIXES)):
+            file_name_dst = PATH_SRC / file_name
+            file_name_dst.write_bytes(path.read_bytes())
 
-        _file_name = ''
-
-        if file_name.endswith(SUFFIX) and not file_name.startswith(PREFIXES):
-            print(Path(root).joinpath(file_name))
-            shutil.copy2(
-                Path(root).joinpath(file_name),
-                PATH_SRC.joinpath(file_name)
-            )
-
-        if not PATH_DST.joinpath(_file_name).exists():
-            shutil.move(
-                Path(root).joinpath(file_name),
-                PATH_DST.joinpath(_file_name)
-            )
+        file_name_dst = PATH_DST / file_name
+        if not file_name_dst.exists():
+            file_name_dst.write_bytes(path.read_bytes())
+            path.unlink()
