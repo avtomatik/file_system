528d79d1284211c0c7d58c85d7d310f338336046
Alexander Mikhailov <extroper@yandex.ru>
2025-06-24 21:31:24 +0300

    Isolated Adapters

--- fileworks/main.py
+++ fileworks/main.py
@@ -1,38 +1,10 @@
 import argparse
 from pathlib import Path
 
-from .interfaces.protocols import FileTransformer
+from .interfaces.adapters import FileMoverAdapter
 from .tools.cleaners import CyrillicToLatinTransliterator, RegexStringCleaner
 from .tools.filters import FileExtensionFilter, NullFileFilter
-from .tools.movers import FileMoverRenamer
 from .tools.transformers import TrimFileNameTransformer
-
-
-class TrimFileNameTransformerAdapter:
-    """Adapter to make TrimFileNameTransformer compatible with FileTransformer
-    protocol."""
-
-    def __init__(self, transformer: TrimFileNameTransformer):
-        self.transformer = transformer
-
-    def transform(self, file_name: str) -> str:
-        return self.transformer.transform(file_name)
-
-
-class FileMoverAdapter:
-    """Adapter to wrap FileMoverRenamer with the required transformer."""
-
-    def __init__(self, transformer: FileTransformer):
-        self.transformer = transformer
-
-    def move_and_rename(
-        self,
-        src_dir: Path,
-        dst_dir: Path,
-        file_names: list[str]
-    ) -> None:
-        mover = FileMoverRenamer(self.transformer)
-        mover.move_and_rename(src_dir, dst_dir, file_names)
 
 
 def main():
